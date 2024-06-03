3284015eb380b1ed1301b5ca2b794defc6774c21
Nilay-Chaudhary <nilaychaudhary04@gmail.com>
2024-06-04 04:06:42 +0530

    ui done

--- src/components/DisplayRouter.jsx
+++ src/components/DisplayRouter.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
+import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const DisplayRouter = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumID = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumID)].bgColor;  // Number(albumID) gives 0 on non-album pages
+
+  useEffect(() => {
+    if(isAlbum){
+      displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
+    }
+    else{
+      displayRef.current.style.background = '#121212'
+    }
+  })
+
   return (
-    <div className='w--[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 scrollbar-none'>
+    <div ref={displayRef} className='w--[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 scrollbar-none'>
         <Routes>
             <Route path='/' element = {<DisplayHome />}> </Route>
-            
+            <Route path='/album/:id' element = {<DisplayAlbum />}> </Route>
         </Routes>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <>
         {/* top row */}
         <div className='w-full flex justify-between items-center font-semibold'>
             <div className='flex items-center gap-2'>
-                <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:scale-[110%]' src={assets.arrow_left} alt="" />
-                <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:scale-[110%]' src={assets.arrow_right} alt="" />
+                <img onClick={() => navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:scale-[110%]' src={assets.arrow_left} alt="" />
+                <img onClick={() => navigate(1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:scale-[110%]' src={assets.arrow_right} alt="" />
             </div>
             <div className='flex items-center gap-4'>
                 <button className='bg-white text-black text-[13px] rounded-2xl px-4 py-1 font-bold hover:scale-[103%]'>Explore Premium</button>
